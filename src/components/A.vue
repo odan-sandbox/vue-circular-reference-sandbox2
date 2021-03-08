50955df2991e9d3f76241cd5776c5832f0802b31
odan <odan3240@gmail.com>
2021-03-08 22:41:01 +0900

    再現

---
+++ src/components/A.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>A</div>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+import B from "./B.vue";
+
+export default defineComponent({
+  components: { B },
+});
+</script>
---
+++ src/components/B.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>B</div>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+import A from "./A.vue";
+
+export default defineComponent({
+  components: { A },
+});
+</script>
